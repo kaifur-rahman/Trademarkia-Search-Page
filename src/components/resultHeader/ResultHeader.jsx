3c6bbcc640c37a62c1ff5d054147b3dd7b620f98
Kaifur Rahman <rahmankaifur8@gmail.com>
2024-09-15 11:26:09 +0530

    urgent fix

--- src/components/resultHeader/ResultHeader.jsx
+++ src/components/resultHeader/ResultHeader.jsx
@@ -1,16 +1,20 @@
 import { useContext } from "react";
 import Box from "@mui/material/Box";
+import SortIcon from "@mui/icons-material/Sort";
+import IconButton from "@mui/material/IconButton";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
 import { SearchContext } from "../contexts/SearchContext";
-
+import { colorScheme } from "../../constants/colorScheme";
+import ResponsiveFilterNav from "../filters/ResponsiveFilterNav";
 export default function ResultHeader() {
   const { totalCount, filters, loading, error } = useContext(SearchContext);
 
   return (
     <Box
       sx={{
-        height: "4rem",
+        height: "5rem",
         alignItems: { xs: "flex-start", md: "center" },
         p: "0.1rem",
         display: "flex",
@@ -46,6 +50,40 @@ export default function ResultHeader() {
             }”`}
           </Typography>
         )}
+      </Box>
+      {/* Result tools */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: { xs: "100%", md: "auto" },
+        }}
+      >
+        {/* Filter */}
+        <ResponsiveFilterNav />
+        {/* Share and Sort */}
+        <Box>
+          {/* Share */}
+          <IconButton
+            size="small"
+            aria-label="copy"
+            sx={{
+              border: "1px solid #C8C8C8",
+              color: colorScheme.light,
+              mr: "0.5rem",
+            }}
+          >
+            <ShareOutlinedIcon />
+          </IconButton>
+          {/* Sort */}
+          <IconButton
+            size="small"
+            aria-label="sort"
+            sx={{ border: "1px solid #C8C8C8", color: colorScheme.light }}
+          >
+            <SortIcon />
+          </IconButton>
+        </Box>
       </Box>
     </Box>
   );
